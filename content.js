a561b7b72208e58ba1bb54182189fb22dfa99684
rsambouncer <rsambouncer@gmail.com>
2019-02-03 00:59:48 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
-    webview.addEventListener('consolemessage', function(e) {
-        var tmp = document.createElement("P");
-        tmp.innerText = e.message;
-        document.getElementById("consolediv").appendChild(tmp);
-    });
+const webview = document.getElementById("webview");
+
+
+webview.addEventListener('consolemessage', function(e) {
+    var tmp = document.createElement("P");
+    tmp.innerText = e.message;
+    document.getElementById("consolediv").appendChild(tmp);
+});
