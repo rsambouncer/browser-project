7648179a985ab4438e473c6f96ea3836145b89f0
rsambouncer <rsambouncer@gmail.com>
2019-02-03 00:54:33 -0800

    Create content.js

---
+++ content.js
@@ -0,0 +1,5 @@
+    webview.addEventListener('consolemessage', function(e) {
+        var tmp = document.createElement("P");
+        tmp.innerText = e.message;
+        document.getElementById("consolediv").appendChild(tmp);
+    });
